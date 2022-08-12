341c01350cfcf76d6ae12984e9bc0bf76b9d8d81
FullSoYu <hey6410@naver.com>
2022-08-12 17:08:21 +0900

    파라미터 활용한 22-08-12 수업 출력(파라미터 아직 안배움)

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -4,30 +4,30 @@ public class Main {
     public static void main(String[] args) {
 
         자동차 Porsche = new 자동차();
-        Porsche.최고속력 = 230;
         Porsche.번호 = 1;
 
+
         자동차 BMW = new 자동차();
-        BMW.최고속력 = 210;
         BMW.번호 = 2;
 
-        Porsche.속력();
-        BMW.속력();
 
+        //1번 자동차가 230km/h 로 달립니다.
+        //2번 자동차가 210km/h 로 달립니다.
 
-
+        Porsche.달리다(230);
+        BMW.달리다(210);
     }
 }
 
 class 자동차 {
 
-    int 최고속력;
     int 번호;
-    void 속력 (){
-        System.out.println(번호 +"번" + " 자동차가 최고속력 " + 최고속력  + "km로 달립니다.");
+
+    void 달리다(int 최고속력){
+
+        System.out.println(번호 + "번 자동차가 최고속력 " + 최고속력 + "km/h 으로 달립니다.");
     }
 
 }
-
 
 
