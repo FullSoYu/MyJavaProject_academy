dc735f1eccfee36a34c4653db5996ec9c7b30121
FullSoYu <hey6410@naver.com>
2022-08-16 16:48:26 +0900

    22.8.16 학원 수업 return을 활용한 간단 계산기

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -3,35 +3,37 @@ package com.example.MyJavaProject;
 public class Main {
     public static void main(String[] args) {
 
+        int 결과;
 
-   계산기.더한결과를_화면에_출력(10,20);
+        결과 = 계산기.합(10, 20);
+        System.out.println("결과 : " + 결과);
 
-   계산기.더한결과를_화면에_출력(50,20);
+        결과 = 계산기.합(30, 20);
+        System.out.println("결과 : " + 결과);
 
-   계산기.뺀결과를_화면에_출력(50,20);
+        결과 = 계산기.합(30, 70);
+        System.out.println("결과 : " + 결과);
 
-   계산기.뺀결과를_화면에_출력(5,2);
+        결과 = 계산기.차(30, 70);
+        System.out.println("결과 : " + 결과);
 
-   계산기.곱한결과를_화면에_출력(5,2);
+        결과 = 계산기.곱(3, 7);
+        System.out.println("결과 : " + 결과);
 
-   계산기.나눗셈을_화면에_출력(5,2);
     }
 }
 
 class 계산기 {
-   static void 더한결과를_화면에_출력 (int i, int j){
-       System.out.println("덧셈 출력 = > 결과 : " + (i + j));
+
+   static int 합 (int i, int j){
+      return  i + j;
+   }
+
+    static int 차(int i, int j){
+        return  i - j;
     }
 
-    static void 뺀결과를_화면에_출력(int i, int j){
-        System.out.println("뺄셈 출력 = > 결과 : " + (i - j));
-    }
-
-    static  void 곱한결과를_화면에_출력(int i, int j){
-        System.out.println("곱셈 출력 = > 결과 : " + (i * j));
-
-    }
-    static void 나눗셈을_화면에_출력(float i, float j){
-        System.out.println("나눗셈 출력 = > 결과 : " + (i / j));
+    static int 곱(int i, int j){
+        return  i * j;
     }
 }
