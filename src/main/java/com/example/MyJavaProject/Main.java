dca9c2381b4db25ccc653b2ab573d16ed960932f
FullSoYu <hey6410@naver.com>
2022-08-19 17:29:45 +0900

    22-08-19 3번 문제

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -1,36 +1,92 @@
 package com.example.MyJavaProject;
 
-
-public class Main {
+class Main {
     public static void main(String[] args) {
+        샤오미Tv a샤오미Tv = new 샤오미Tv();
+        삼성Tv a삼성Tv = new 삼성Tv();
+        LGTv aLGTv = new LGTv();
 
-        전사 a전사 = new 전사();
+        a샤오미Tv.켜기();
+        // 출력 => 샤오미Tv 켜집니다.
+        a샤오미Tv.끄기();
+        // 출력 => 샤오미Tv 꺼집니다.
+        a샤오미Tv.vr켜기();
+        // 출력 => 샤오미Tv vr켜기!
 
-        a전사.공격("브라이언", "칼");
-        // 출력 : 브라이언(이)가 칼(으)로 공격합니다. 파라미터 사용
-        a전사.재공격();
-        // 출력 : 브라이언(이)가 칼(으)로 공격합니다. 파라미터 사용 불가
-        a전사.공격("필립", "창");
-        // 출력 : 필립(이)가 창(으)로 공격합니다. 파라미터 사용
-        a전사.공격("마크", "지팡이");
-        // 출력 : 마크(이)가 지팡이(으)로 공격합니다. 파라미터 사용
-        a전사.재공격();
-        // 출력 : 마크(이)가 지팡이(으)로 공격합니다. 파라미터 사용 불가
-        a전사.재공격();
-        // 출력 : 마크(이)가 지팡이(으)로 공격합니다. 파라미터 사용 불가
+        a삼성Tv.켜기();
+        // 출력 => 삼성Tv 켜집니다.
+        a삼성Tv.끄기();
+        // 출력 => 삼성Tv 꺼집니다.
+        a삼성Tv.ar켜기();
+        // 출력 => 삼성Tv ar켜기!
+
+        aLGTv.켜기();
+        // 출력 => LGTv 켜집니다.
+        aLGTv.끄기();
+        // 출력 => LGTv 꺼집니다.
+        aLGTv.게임모드전환();
+        // 출력 => LGTv 게임모드전환!
+
+        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
+        표준Tv a표준Tv;
+        //샤오미 Tv 리모콘 표준 Tv 리모콘 연결
+        a표준Tv.켜기();
+        // 출력 : 샤오미TV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : 샤오미TV 꺼집니다.
+
+        //삼성 Tv 리모콘 표준 Tv 리모콘 연결
+        a표준Tv.켜기();
+        // 출력 : 삼성 TV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : 삼성 TV 꺼집니다.
+
+        a표준Tv.켜기();
+        // 출력 : LGTV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : LGTV 꺼집니다.
+
+        // 해볼사람만 해보세요
+        //a표준Tv로 각tv의 특수기능 사용하기
+
     }
 }
 
-class 전사 {
-    String 이름;
-    String 무기;
-    void 공격(String 이름, String 무기) {
-        this.이름 = 이름;
-        this.무기 = 무기;
-        재공격();
+class 샤오미Tv {
+    void 켜기() {
+        System.out.println("샤오미Tv 켜집니다.");
     }
-    void 재공격(){
-        System.out.printf("%s(이)가 %s(으)로 공격합니다.%n", 이름, 무기);
+
+    void 끄기() {
+        System.out.println("샤오미Tv 꺼집니다.");
+    }
+    void vr켜기() {
+        System.out.println("샤오미Tv vr켜기!");
     }
 }
 
+class 삼성Tv {
+    void 켜기() {
+        System.out.println("삼성Tv 켜집니다.");
+    }
+
+    void 끄기() {
+        System.out.println("삼성Tv 꺼집니다.");
+    }
+    void ar켜기() {
+        System.out.println("삼성Tv ar켜기!");
+    }
+}
+
+class LGTv {
+    void 켜기() {
+        System.out.println("LGTv 켜집니다.");
+    }
+
+    void 끄기() {
+        System.out.println("LGTv 꺼집니다.");
+    }
+    void 게임모드전환() {
+        System.out.println("LGTv 게임모드전환!");
+    }
+}
