029c646d2d44a0ac3962cdef4ad89e8ee78a70c6
FullSoYu <hey6410@naver.com>
2022-08-19 15:44:10 +0900

    22-08-19 1번 문제

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -4,16 +4,16 @@ package com.example.MyJavaProject;
 public class Main {
     public static void main(String[] args) {
 
-        칼 a칼 = new 칼();
-        a칼 = new 활();
+    전사 a전사 = new 전사();
 
+    a전사.공격();
+    // 출력 : 브라이언(이)가 칼(으)로 공격합니다. 파라미터 사용
+    a전사.공격();
+    // 출력 : 필립(이)가 창(으)로 공격합니다. 파라미터 사용
+    a전사.공격();
+    // 출력 : 마크(이)가 지팡이(으)로 공격합니다. 파라미터 사용
     }
 }
 
-class 칼 {
 
-}
 
-class 활 extends 칼{
-
-}
