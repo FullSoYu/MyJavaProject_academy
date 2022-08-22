c2ced6491742349dd2c63f4ffff1dd195726656d
FullSoYu <hey6410@naver.com>
2022-08-22 15:17:13 +0900

    22-08-22 1번 정답

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -2,7 +2,7 @@ package com.example.MyJavaProject;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // 변수 갯수 1개 a사람
         사람 a사람 = new 사람();
         a사람.나이 = 20;
         a사람.a왼팔 = new 팔();
@@ -10,3 +10,12 @@ public class Main {
     }
 }
 
+class 사람 {
+        int 나이;
+
+        팔 a왼팔;
+}
+
+class 팔 {
+
+}
