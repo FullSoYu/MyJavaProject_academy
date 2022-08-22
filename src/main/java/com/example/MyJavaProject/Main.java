1808e74f2ab304e1b06f9789e704cbdc659c45b4
FullSoYu <hey6410@naver.com>
2022-08-22 16:51:06 +0900

    22-08-22 4번 정답 생성자 오버로딩

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -10,11 +10,25 @@ public class Main {
         a사람.이름 = "홍길동";
         System.out.printf("저는 %d번 사람 %s입니다.%n", a사람.번호, a사람.이름);
 
-
+        사람 b사람 = new 사람(0,"무명");
+        System.out.printf("저는 %d번 사람 %s입니다.%n", b사람.번호, b사람.이름);
     }
 }
 
 class 사람 {      //초기 값 넣어주기 단,
     int 번호;         //int = -1
     String 이름;      //String = "이름 없음" 이렇게 하지말고 풀어보기
+
+    //생성자 생성
+    사람(){
+        this.이름 = "이름 없음";
+        this.번호 = -1;
+    }
+    //생성자 오버로딩
+    사람(int 번호, String 이름){
+        this.번호 = 번호;
+        this.이름 = 이름;
+    }
+
+
 }
