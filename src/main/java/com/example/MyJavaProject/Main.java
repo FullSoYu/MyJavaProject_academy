60227e3009278f5122423bdb853bda460b1d9890
FullSoYu <hey6410@naver.com>
2022-08-22 15:24:52 +0900

    22-08-22 2번 문제

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -3,19 +3,12 @@ package com.example.MyJavaProject;
 
 public class Main {
     public static void main(String[] args) { // 변수 갯수 1개 a사람
-        사람 a사람 = new 사람();
-        a사람.나이 = 20;
-        a사람.a왼팔 = new 팔();
+
+        전사 a전사 = new 전사();
+        a전사.나이 = 20;
+        a전사.a무기 = new 활();
+        a전사.a무기 = new 칼();
 
     }
 }
 
-class 사람 {
-        int 나이;
-
-        팔 a왼팔;
-}
-
-class 팔 {
-
-}
