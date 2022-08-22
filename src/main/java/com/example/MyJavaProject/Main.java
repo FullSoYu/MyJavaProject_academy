ccd52969e289aa1cb1c600705a73d51244755980
FullSoYu <hey6410@naver.com>
2022-08-22 15:13:52 +0900

    22-08-22 1번 문제

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -1,92 +1,12 @@
 package com.example.MyJavaProject;
 
-class Main {
+
+public class Main {
     public static void main(String[] args) {
-        샤오미Tv a샤오미Tv = new 샤오미Tv();
-        삼성Tv a삼성Tv = new 삼성Tv();
-        LGTv aLGTv = new LGTv();
-
-        a샤오미Tv.켜기();
-        // 출력 => 샤오미Tv 켜집니다.
-        a샤오미Tv.끄기();
-        // 출력 => 샤오미Tv 꺼집니다.
-        a샤오미Tv.vr켜기();
-        // 출력 => 샤오미Tv vr켜기!
-
-        a삼성Tv.켜기();
-        // 출력 => 삼성Tv 켜집니다.
-        a삼성Tv.끄기();
-        // 출력 => 삼성Tv 꺼집니다.
-        a삼성Tv.ar켜기();
-        // 출력 => 삼성Tv ar켜기!
-
-        aLGTv.켜기();
-        // 출력 => LGTv 켜집니다.
-        aLGTv.끄기();
-        // 출력 => LGTv 꺼집니다.
-        aLGTv.게임모드전환();
-        // 출력 => LGTv 게임모드전환!
-
-        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
-        표준Tv a표준Tv;
-        //샤오미 Tv 리모콘 표준 Tv 리모콘 연결
-        a표준Tv.켜기();
-        // 출력 : 샤오미TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 샤오미TV 꺼집니다.
-
-        //삼성 Tv 리모콘 표준 Tv 리모콘 연결
-        a표준Tv.켜기();
-        // 출력 : 삼성 TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 삼성 TV 꺼집니다.
-
-        a표준Tv.켜기();
-        // 출력 : LGTV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : LGTV 꺼집니다.
-
-        // 해볼사람만 해보세요
-        //a표준Tv로 각tv의 특수기능 사용하기
+        사람 a사람 = new 사람();
+        a사람.나이 = 20;
+        a사람.a왼팔 = new 팔();
 
     }
 }
 
-class 샤오미Tv {
-    void 켜기() {
-        System.out.println("샤오미Tv 켜집니다.");
-    }
-
-    void 끄기() {
-        System.out.println("샤오미Tv 꺼집니다.");
-    }
-    void vr켜기() {
-        System.out.println("샤오미Tv vr켜기!");
-    }
-}
-
-class 삼성Tv {
-    void 켜기() {
-        System.out.println("삼성Tv 켜집니다.");
-    }
-
-    void 끄기() {
-        System.out.println("삼성Tv 꺼집니다.");
-    }
-    void ar켜기() {
-        System.out.println("삼성Tv ar켜기!");
-    }
-}
-
-class LGTv {
-    void 켜기() {
-        System.out.println("LGTv 켜집니다.");
-    }
-
-    void 끄기() {
-        System.out.println("LGTv 꺼집니다.");
-    }
-    void 게임모드전환() {
-        System.out.println("LGTv 게임모드전환!");
-    }
-}
