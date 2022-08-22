797fcb7f38455881b1ae928909cc87887972159d
FullSoYu <hey6410@naver.com>
2022-08-22 16:31:04 +0900

    22-08-22 4번 문제

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -2,30 +2,19 @@ package com.example.MyJavaProject;
 
 
 public class Main {
-    public static void main(String[] args) { // 변수 갯수 1개 a사람
+    public static void main(String[] args) {
+        사람 a사람 = new 사람();
+        System.out.printf("저는 %d번 사람 %s입니다.%n", a사람.번호, a사람.이름);
+        // 저는 -1번 사람 이름없음입니다.
+        a사람.번호 = 1;
+        a사람.이름 = "홍길동";
+        System.out.printf("저는 %d번 사람 %s입니다.%n", a사람.번호, a사람.이름);
 
-        전사 a전사 = new 전사();
-        a전사.나이 = 20;
-        a전사.a무기 = new 활();
-        a전사.a무기 = new 칼();
 
     }
 }
 
-class 전사 {
-    int 나이;
-
-   무기 a무기;
-}
-
-class 무기 {
-
-}
-
-class 활 extends 무기 {
-
-}
-
-class 칼 extends 무기{
-
+class 사람 {      //초기 값 넣어주기 단,
+    int 번호;         //int = -1
+    String 이름;      //String = "이름 없음" 이렇게 하지말고 풀어보기
 }
