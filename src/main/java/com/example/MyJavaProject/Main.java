cce4780152cc37f312e2105a5a22e699fff1ba79
FullSoYu <hey6410@naver.com>
2022-08-22 17:25:37 +0900

    22-08-22 3번 public 없이 정답

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -4,7 +4,7 @@ package com.example.MyJavaProject;
 public class Main {
     public static void main(String[] args) {
         전사 a전사 = new 전사();
-        a전사.이름 = 이름;
+        a전사.이름 = "칸";
         a전사.나이 = 20;
         a전사.자기소개();
         // 안녕하세요. 저는 20살 칸입니다.
@@ -23,6 +23,44 @@ public class Main {
         // 출력 : 철수가 칼로 공격합니다.
 
 
+    }
+}
+
+class 전사 {
+    String 이름;
+    int 나이;
+
+    무기 a무기;
+
+    void 자기소개 (){
+        System.out.printf("안녕하세요 저는 %d살 %s입니다.%n", 나이, 이름);
+    }
+
+    void 공격 (){
+        this.a무기.공격(이름);
+    }
+}
+
+class 무기 {
+    void 공격 (String 이름) {
+        System.out.println(이름 + "(이)가 공격합니다.");
+    }
+
+}
+
+
+class 칼 extends 무기 {
+
+    void 공격 (String 이름) {
+        System.out.println(이름 + "(이)가 칼로 공격합니다.");
+    }
+    
+}
+
+class 활 extends 무기 {
+
+    void 공격 (String 이름) {
+        System.out.println(이름 + "(이)가 활로 공격합니다.");
     }
 }
 
