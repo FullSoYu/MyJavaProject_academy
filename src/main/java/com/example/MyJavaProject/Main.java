42768d1b3c499272a8cb394066137d590f423421
FullSoYu <hey6410@naver.com>
2022-08-24 17:42:53 +0900

    22-08-23 2번 문제 정답(생성자 사용)

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -21,4 +21,46 @@ class Main {
 
     }
 }
+class 전사 {
+    String 이름;
+    int 나이;
 
+    무기 a무기;
+
+    void 자기소개() {
+        System.out.printf("안녕하세요 저는 %d살 %s입니다.\n", 나이, 이름);
+    }
+
+    void 공격(){
+        a무기.전사이름 = this.이름;
+        a무기.무기로_공격();
+
+    }
+
+}
+
+class 무기 {
+    String 장비;
+    String 전사이름;
+    void 무기로_공격() {
+        System.out.printf("%s(이)가 %s로 공격합니다.\n", 전사이름, 장비);
+    }
+
+}
+
+
+class 칼 extends 무기 {
+
+    칼() {
+        장비 = "칼";
+    }
+
+}
+
+class 활 extends 무기 {
+
+    활() {
+        장비 = "활";
+    }
+
+}
