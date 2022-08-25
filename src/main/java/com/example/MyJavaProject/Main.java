d6e67904a97f644587b105c992b6405373c8b97a
FullSoYu <hey6410@naver.com>
2022-08-25 15:22:56 +0900

    22-08-24 1번 문제 arraylist 관련문제

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -3,6 +3,7 @@ package com.example.MyJavaProject;
 class Main {
     public static void main(String[] args) {
 
+        사람 a사람인력관리소 = new 사람인력관리소();
 
         a사람인력관리소.add사람("홍길순", 33);
         // 나이가 33살인 1번째 사람(홍길순)이 추가되었습니다.
@@ -24,3 +25,38 @@ class Main {
         // 저는 3번, 임꺽정, 30살 입니다.
     }
 }
+
+abstract class 사람 {
+
+    String 이름;
+
+    int 나이;
+
+    사람인력관리소 a사람인력관리소;
+
+    void add사람(String 이름, int 나이) {
+
+        a사람인력관리소.명단("이름", 나이);
+    }
+
+    void 자기소개() {
+        
+    }
+    
+}
+
+class 직업 extends 사람 {
+
+
+    void 명단 (String 이름, int 나이) {
+        String[] d = this.이름;
+    }
+    
+}
+
+
+
+class 사람인력관리소 extends 직업 {
+    
+}
+
