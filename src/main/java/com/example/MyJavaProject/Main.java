cd849dcb96199b8ce84df1eb41e705c7ea3f5276
FullSoYu <hey6410@naver.com>
2022-08-26 15:26:52 +0900

    22-08-26 2번 문제

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -3,6 +3,11 @@ package com.example.MyJavaProject;
 class Main {
     public static void main(String[] args) {
 
+
+        // 문제 : 아래와 같이 출력되도록 해주세요.
+        // 조건 : 클래스는 전사 클래스 1개만 만들어주세요.
+        // 조건 : 무기 이름을 넣는 변수 없이 풀어주세요.
+
         전사 a전사 = new 전사();
 
         a전사.공격();
@@ -21,26 +26,4 @@ class Main {
 
     }
 }
-
-class 전사 {
-    String 무기_이름;
-
-    전사() {
-        무기_이름 = "칼";
-    }
-
-    void 공격() {
-        System.out.printf("%s(으)로 공격합니다.%n", 무기_이름);
-    }
-
-    void 창_모드로_변경(){
-        무기_이름 = "창";
-        System.out.printf("%s(으)로 공격합니다.%n", 무기_이름);
-    }
-    void 지팡이_모드로_변경() {
-        무기_이름 = "지팡이";
-        System.out.printf("%s(으)로 공격합니다.%n", 무기_이름);
-    }
-}
-
 
