30a482301aa0409ee38de049dd3532689ed86899
FullSoYu <hey6410@naver.com>
2022-08-26 16:28:13 +0900

    22-08-26 4번 문제 내가 푼 정답

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -21,3 +21,64 @@ class Main {
     }
 }
 
+class 전사 {
+
+    String 이름;
+
+    무기 a무기;
+
+    void 공격() {
+        System.out.printf("%s(이)가 %s(으)로 공격합니다.%n", 이름, a무기.무기이름);
+    }
+
+}
+
+class 홍길동 extends 전사 {
+
+    홍길동() {
+        이름 = "홍길동";
+    }
+
+}
+
+class 홍길순 extends 전사 {
+
+    홍길순() {
+
+        이름 = "홍길순";
+        a무기 = new 칼();
+    }
+
+
+}
+
+class 무기 {
+
+    String 무기이름;
+
+}
+
+class 활 extends 무기 {
+
+    활() {
+        무기이름 = "활";
+    }
+
+}
+
+class 창 extends 무기 {
+
+    창() {
+        무기이름 = "창";
+
+    }
+
+}
+
+class 칼 extends 무기 {
+    칼() {
+        무기이름 = "칼";
+
+    }
+
+}
