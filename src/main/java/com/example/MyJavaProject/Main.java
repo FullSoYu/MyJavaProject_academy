8c534836cbfcbd62734b18a712a80ab173a66e47
FullSoYu <hey6410@naver.com>
2022-08-29 15:14:26 +0900

    22-08-29 1번 문제

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -3,82 +3,48 @@ package com.example.MyJavaProject;
 class Main {
     public static void main(String[] args) {
 
-        전사 a전사 = new 홍길동();
-        a전사.a무기 = new 활();
 
-        a전사.공격();
-        // 출력 => 홍길동이(가) 활(으)로 공격합니다.
+        청둥오리 a청둥오리 = new 청둥오리();
+        a청둥오리.날다();
+        // 출력 : 오리가 날개로 날아갑니다.
 
-        a전사 = new 홍길순();
+        흰오리 a흰오리 = new 흰오리();
+        a흰오리.날다();
+        // 출력 : 오리가 날개로 날아갑니다.
 
-        a전사.공격();
-        // 출력 => 홍길순이(가) 칼(으)로 공격합니다.
+        고무오리 a고무오리 = new 고무오리();
+        a고무오리.날다();
+        // 출력 : 저는 날 수 없어요. ㅜㅠ
 
-        a전사.a무기 = new 창();
-        a전사.공격();
-        // 출력 => 홍길순이(가) 창(으)로 공격합니다.
-
+        고무2오리 a고무2오리 = new 고무2오리();
+        a고무2오리.날다();
+        // 출력 : 저는 날 수 없어요. ㅜㅠ
     }
 }
 
-class 전사 {
-
-    String 이름;
-
-    무기 a무기;
-
-    void 공격() {
-        System.out.printf("%s(이)가 %s(으)로 공격합니다.%n", 이름, a무기.무기이름);
+class 오리 {
+    void 날다() {
+        System.out.println("오리가 날개로 날아갑니다.");
     }
+}
+
+class 흰오리 extends 오리 {
 
 }
 
-class 홍길동 extends 전사 {
-
-    홍길동() {
-        이름 = "홍길동";
-    }
+class 청둥오리 extends 오리 {
 
 }
 
-class 홍길순 extends 전사 {
+class 고무오리 extends 오리 {
 
-    홍길순() {
-
-        이름 = "홍길순";
-        a무기 = new 칼();
+    void 날다() {
+        System.out.println("저는 날 수 없어요. ㅜㅠ");
     }
-
-
 }
 
-class 무기 {
-
-    String 무기이름;
-
-}
-
-class 활 extends 무기 {
-
-    활() {
-        무기이름 = "활";
+class 고무2오리 extends 오리 {
+    void 날다() {
+        System.out.println("저는 날 수 없어요. ㅜㅠ");
     }
-
-}
-
-class 창 extends 무기 {
-
-    창() {
-        무기이름 = "창";
-
-    }
-
-}
-
-class 칼 extends 무기 {
-    칼() {
-        무기이름 = "칼";
-
-    }
-
 }
