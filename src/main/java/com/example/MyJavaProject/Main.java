1e89a6bdfe89f4f6bdac171c2f010f6e92bfaca9
FullSoYu <hey6410@naver.com>
2022-09-13 17:09:41 +0900

    09-13 5번 문제

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -16,13 +16,54 @@ class Main {
         // 출력 : 로봇이 진찰합니다.
         진찰하다(new 삼성());
         // 출력 : 삼성이 진찰합니다.
+        진찰하다(new LG());
+        // 출력 : 삼성이 진찰합니다.
     }
 
-    public static void 진찰하다(Object obj) {
-        // 구현 시작
-        // 구현 끝
+    // 여기에 static 추가
+    public static void 진찰하다(의사 a의사) {
+        a의사.진찰();
     }
 }
 
-// 구현 시작
-// 구현 끝
+class 사람 {
+    public void 진료() {
+        System.out.println("사람이 진찰합니다.");
+    }
+}
+
+class 원숭이 {
+    public void 살펴보다() {
+        System.out.println("원숭이가 진찰합니다.");
+    }
+}
+
+class 치타 {
+    public void 진찰() {
+        System.out.println("치타가 진찰합니다.");
+    }
+}
+
+class 기린 {
+    public void 진찰() {
+        System.out.println("기린이 진찰합니다.");
+    }
+}
+
+class 로봇 {
+    public void 오류감지() {
+        System.out.println("로봇이 진찰합니다.");
+    }
+}
+
+class 삼성 {
+    public void 삼성병원에_진찰하라고_지시() {
+        System.out.println("삼성이 진찰합니다.");
+    }
+}
+
+class LG {
+    public void 삼성병원에_진찰하라고_지시() {
+        System.out.println("LG가 진찰합니다.");
+    }
+}
