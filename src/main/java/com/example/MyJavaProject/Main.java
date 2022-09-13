a031412c9e19a7d43b7725bb1be430eb518e9079
FullSoYu <hey6410@naver.com>
2022-09-13 16:23:30 +0900

    09-13 4번 문제

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -4,57 +4,25 @@ package com.example.MyJavaProject;
 
 class Main {
     public static void main(String[] args) {
-        사람 a사람 = new 사람();
+        진찰하다(new 사람());
+        // 출력 : 사람이 진찰합니다.
+        진찰하다(new 원숭이());
+        // 출력 : 원숭이가 진찰합니다.
+        진찰하다(new 치타());
+        // 출력 : 치타가 진찰합니다.
+        진찰하다(new 기린());
+        // 출력 : 기린이 진찰합니다.
+        진찰하다(new 로봇());
+        // 출력 : 로봇이 진찰합니다.
+        진찰하다(new 삼성());
+        // 출력 : 삼성이 진찰합니다.
+    }
 
-        변호사 a변호사 = a사람;
-        a변호사.변호하다();
-        // 출력 : 사람이 변호 합니다.
-
-        변호사 a변호사2 = new 오랑우탄();
-        a변호사2.변호하다();
-        // 출력 : 오랑우탄이 변호 합니다.
-
-        의사 a의사 = new 오랑우탄();
-        a의사.진찰하다();
-        // 출력 : 오랑우탄이 진찰 합니다.
-
-        의사 a의사2 = new 사람();
-        a의사2.진찰하다();
-        // 출력 : 사람이 진찰 합니다.
+    public static void 진찰하다(Object obj) {
+        // 구현 시작
+        // 구현 끝
     }
 }
 
-class 사람 implements 변호사, 의사 {
-
-    public void 변호하다() {
-        System.out.println("사람이 변호 합니다.");
-    }
-
-    public void 진찰하다() {
-        System.out.println("사람이 진찰 합니다.");
-    }
-
-}
-
-interface 변호사 {
-
-    void 변호하다();
-
-}
-
-class 오랑우탄 implements 변호사, 의사{
-
-    public void 변호하다() {
-        System.out.println("오랑우탄이 변호 합니다.");
-    }
-
-    public void 진찰하다() {
-        System.out.println("오랑우탄이 진찰 합니다.");
-    }
-
-}
-
-interface 의사 {
-    void 진찰하다();
-}
-
+// 구현 시작
+// 구현 끝
