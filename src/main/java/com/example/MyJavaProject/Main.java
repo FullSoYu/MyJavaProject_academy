329c37f4ebdb9a9ff160eba843b6878242d30ecb
FullSoYu <hey6410@naver.com>
2022-09-13 15:44:54 +0900

    09-13 2번 문제

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -5,8 +5,9 @@ package com.example.MyJavaProject;
 class Main {
     public static void main(String[] args) {
         사람 a사람 = new 홍길동();
-
-
         변호사 a변호사 = (변호사) a사람;
+        치과의사 a치과의사 = (치과의사) a사람;
+        성화봉송자 a성화봉송자 = (성화봉송자) a사람;
     }
 }
+
