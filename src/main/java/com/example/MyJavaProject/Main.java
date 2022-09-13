0a62ba53219ae2a3023710fa8313783d1e420b99
FullSoYu <hey6410@naver.com>
2022-09-13 17:25:00 +0900

    09-13 5번 문제 정답

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -1,6 +1,13 @@
 
 package com.example.MyJavaProject;
 
+// 문제 : 아래가 실행되도록 해주세요.
+// 조건 : Main 클래스는 수정 할 수 없습니다.
+// 조건 : 각 클래스의 기존 메서드는 삭제/수정 할 수 없습니다.
+// 조건 : 그 외의 모든 것은 전부 수정가능합니다.
+// 조건 : 소스코드 중복이 있으면 안됩니다.
+
+
 
 class Main {
     public static void main(String[] args) {
@@ -26,44 +33,81 @@ class Main {
     }
 }
 
-class 사람 {
+class 사람 implements 의사 {
     public void 진료() {
         System.out.println("사람이 진찰합니다.");
     }
+
+    @Override
+    public void 진찰() {
+        진료();
+    }
 }
 
-class 원숭이 {
+class 원숭이 implements 의사 {
+
+
     public void 살펴보다() {
         System.out.println("원숭이가 진찰합니다.");
     }
-}
 
-class 치타 {
+    @Override
     public void 진찰() {
-        System.out.println("치타가 진찰합니다.");
+        살펴보다();
     }
 }
 
-class 기린 {
+class 치타  implements 의사{
+
+    @Override
+    public void 진찰() {
+        System.out.println("치타가 진찰합니다.");
+    }
+
+}
+
+class 기린 implements 의사 {
+
+    @Override
     public void 진찰() {
         System.out.println("기린이 진찰합니다.");
     }
 }
 
-class 로봇 {
+class 로봇 implements 의사{
     public void 오류감지() {
         System.out.println("로봇이 진찰합니다.");
     }
+    @Override
+    public void 진찰(){
+        오류감지();
+    }
 }
 
-class 삼성 {
+class 삼성 implements 의사 {
     public void 삼성병원에_진찰하라고_지시() {
         System.out.println("삼성이 진찰합니다.");
     }
+
+    @Override
+    public void 진찰() {
+        삼성병원에_진찰하라고_지시();
+    }
 }
 
-class LG {
+class LG implements 의사{
     public void 삼성병원에_진찰하라고_지시() {
         System.out.println("LG가 진찰합니다.");
     }
+
+    @Override
+    public void 진찰() {
+        삼성병원에_진찰하라고_지시();
+    }
+}
+
+interface 의사 {
+
+    void 진찰();
+
 }
