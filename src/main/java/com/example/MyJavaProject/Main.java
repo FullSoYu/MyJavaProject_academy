2b3ba6e1d0aaf76d603d2464a3a8bfd652e93bdb
FullSoYu <hey6410@naver.com>
2022-09-14 16:01:51 +0900

    09-14 2번 문제 정답

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -1,16 +1,35 @@
 
 package com.example.MyJavaProject;
 
+// 문제 : 사람의 id를 출력해주세요.
+// 조건 : 사람의 id를 20으로 설정해주세요.
+// 조건 : 사람 클래스의 id 필드의 private 속성은 수정할 수 없습니다.
 class Main {
     public static void main(String[] args) {
         사람 a사람 = new 사람();
-        a사람.id = 20;
+        // 구현시작
 
-        System.out.println("제 번호는 " + a사람.id + " 입니다.");
+        //
+        a사람.setid(20);
+
+        System.out.printf("제 번호는 %d 입니다.\n", a사람.getid());
+        // 구현끝
         // 출력 : 제 번호는 20 입니다.
     }
 }
 
 class 사람 {
-    public int id;
+    private int id;
+
+    // 구현 시작
+    public void setid(int num) {
+        id = num;
+    }
+
+
+    public int getid() {
+        return id;
+    }
+
+    // 구현
 }
