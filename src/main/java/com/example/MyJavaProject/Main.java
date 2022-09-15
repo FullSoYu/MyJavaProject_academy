29b78a6ca4a823583929ecddd8784e8a0f491a9f
FullSoYu <hey6410@naver.com>
2022-09-15 16:10:10 +0900

    09-15 1번 시험 문제

--- src/main/java/com/example/MyJavaProject/Main.java
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -1,35 +1,32 @@
-
 package com.example.MyJavaProject;
 
-// 문제 : 사람의 id를 출력해주세요.
-// 조건 : 사람의 id를 20으로 설정해주세요.
-// 조건 : 사람 클래스의 id 필드의 private 속성은 수정할 수 없습니다.
 class Main {
     public static void main(String[] args) {
-        사람 a사람 = new 사람();
-        // 구현시작
+        사람[] 사람들 = new 사람[7];
+        사람들[0] = 사람.get사람();
+        사람들[1] = 사람.get사람();
+        사람들[2] = 사람.get사람();
+        사람들[3] = 사람.get사람();
+        사람들[4] = 사람.get사람();
+        사람들[5] = 사람.get사람();
+        사람들[6] = 사람.get사람();
+        for ( int i = 0; i < 사람들.length; i++ ) {
+            사람들[i].자기소개();
+        }
 
-        //
-        a사람.setid(20);
 
-        System.out.printf("제 번호는 %d 입니다.\n", a사람.getid());
-        // 구현끝
-        // 출력 : 제 번호는 20 입니다.
+
+
+        		/*
+		// 출력
+		저는 1번째 사람입니다.
+		저는 2번째 사람입니다.
+		저는 3번째 사람입니다.
+		저는 3번째 사람입니다.
+		저는 3번째 사람입니다.
+		저는 3번째 사람입니다.
+		저는 3번째 사람입니다.
+		*/
     }
 }
 
-class 사람 {
-    private int id;
-
-    // 구현 시작
-    public void setid(int num) {
-        id = num;
-    }
-
-
-    public int getid() {
-        return id;
-    }
-
-    // 구현
-}
